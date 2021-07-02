79d88635bcea12e9506cd50bdcc042af362dd9eb
Eric Cole <ecole052785@gmail.com>
2021-07-02 11:36:01 -0400

    cleaned up server file

--- server.js
+++ server.js
@@ -1,30 +1,26 @@
 // dependancies //routes
 const express = require('express');
 const htmlRoutes = require('./routes/htmlRoutes');
-const mongoose = require("mongoose")
-const morgan = require("morgan");
-const apiRoutes = require("./routes/api");
-
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const apiRoutes = require('./routes/api');
 // requires express and sets our host page #
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 // facilitates express / builds middleware
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(htmlRoutes);
-app.use(express.static("./public"));
-app.use(morgan("dev"));
-app.use(apiRoutes);
-
-// connects database
+app.use(express.static('./public'));
 mongoose.connect('mongodb://localhost/workout', {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
 });
-
+app.use(htmlRoutes);
+app.use(apiRoutes);
+// connects database
 // then turn on the server
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+    console.log(`App listening on port ${PORT}!`);
 });
